7a4d29451fffe1684ebb2c356b2aceb2bec5b036
ericbui2@gmail.com <ebui91@Erics-MacBook-Pro.local>
2017-10-24 11:13:29 -0500

    finished React Router assignment

---
+++ src/router.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {Switch, Route} from 'react-router-dom';
+
+import Store from './components/Store/Store.js';
+import Details from './components/Details/Details.js';
+import Checkout from './components/Checkout/Checkout.js'
+
+
+export default(
+    <div>
+      <Switch>
+        <Route exact path='/' component={Store} />
+        <Route path='/details/:id' component={Details} />
+        <Route path='/checkout' component={Checkout} />
+      </Switch>
+    </div>
+  )
